3edc8358f09f6209b0100d7e0ab9bf54f2e5d9a2
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-04-27 18:34:42 +0200

    Update Login

--- login.php
+++ login.php
@@ -2,24 +2,15 @@
 require 'DB.php';
 
 $username = $_POST['username'] ?? '';
-$password = $_POST['hashed_password'] ?? '';
 
-$q = $mysqli->prepare("SELECT user_id FROM Users WHERE username = ? AND hashed_password = ?");
-$q->bind_param("ss", $username, $password);
+$q = $mysqli->prepare("SELECT uuid FROM user WHERE name = ?");
+$q->bind_param("s", $username);
 $q->execute();
 $result = $q->get_result();
 
 if ($result->num_rows === 1) {
     $row = $result->fetch_assoc();
-    $user_id = $row['user_id'];
-    
-    // updte last_connected for fun
-    $today = date('Y-m-d');
-    $update = $mysqli->prepare("UPDATE Users SET last_connected = ? WHERE user_id = ?");
-    $update->bind_param("si", $today, $user_id);
-    $update->execute();
-    
-    echo $user_id;
+    echo $row['uuid'];
 } else {
     echo "FAIL";
 }
