e71b75e1fc3b6eb092172f6b04a3d7ac26d9816f
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-04-27 18:34:02 +0200

    Register update

--- register.php
+++ register.php
@@ -1,12 +1,12 @@
 <?php
 require 'DB.php';
 
+$uuid     = $_POST['uuid'] ?? uniqid('', true);
 $username = $_POST['username'] ?? '';
 $password = $_POST['hashed_password'] ?? '';
-$today = date('Y-m-d');
 
-$q = $mysqli->prepare("INSERT INTO Users (username, hashed_password, last_connected) VALUES (?, ?, ?)");
-$q->bind_param("sss", $username, $password, $today);
+$q = $mysqli->prepare("INSERT INTO user (uuid, name) VALUES (?, ?)");
+$q->bind_param("ss", $uuid, $username);
 if ($q->execute()) {
     echo "REGISTERED";
 } else {
