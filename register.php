369759b8811366276b68c24b843b9b86cce3cd87
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-04-27 20:21:49 +0200

    Added Initial bestscore = 0 to ease things

--- register.php
+++ register.php
@@ -3,7 +3,7 @@ require 'DB.php';
 
 $uuid     = $_GET['uuid'] ?? uniqid('', true);
 $username = $_GET['username'] ?? '';
-$q = $mysqli->prepare("INSERT INTO user (uuid, name) VALUES (?, ?)");
+$q = $mysqli->prepare("INSERT INTO user (uuid, name, bestscore) VALUES (?, ?, 0)");
 $q->bind_param("ss", $uuid, $username);
 if ($q->execute()) {
     echo "REGISTERED:".$uuid;
