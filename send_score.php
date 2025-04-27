eb2486f6420ab882fc1ac67d730e9f92005711f2
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-04-27 20:21:15 +0200

    Change send_score method to use GET parameters and fixed table type

--- send_score.php
+++ send_score.php
@@ -1,10 +1,11 @@
 <?php
 require 'DB.php';
 
-$uuid = $_GET['user_id'] ?? '';
+// TODO: Check if uuid is valid
+$uuid = $_GET['user_id'] ?? $_GET['uuid'] ?? '';
 $score = intval($_GET['score'] ?? 0);
 
-$q = $mysqli->prepare("INSERT INTO scores (uuid, score) VALUES (?, ?)");
+$q = $mysqli->prepare("INSERT INTO score (uuid, score) VALUES (?, ?)");
 $q->bind_param("si", $uuid, $score);
 
 // Update bestscore if necessary
