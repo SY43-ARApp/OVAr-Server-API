7cc3ae9ec6fa93b1c2573824435314c59cf5fcf1
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-05-08 15:14:21 +0200

    UUID/username

--- login.php
+++ login.php
@@ -1,16 +1,28 @@
 <?php
 require 'DB.php';
 
-$username = $_GET['uuid'] ?? '';
+$uuid = $_GET['uuid'] ?? '';
+$username = $_GET['username'] ?? '';
 
-$q = $mysqli->prepare("SELECT 'GOOD' FROM user WHERE uuid = '?'");
-$q->bind_param("s", $username);
+if($username) {
+    $q = $mysqli->prepare("SELECT uuid FROM user WHERE username = '?'");
+    $q->bind_param("s", $username);
+} else {
+    $q = $mysqli->prepare("SELECT 'GOOD' FROM user WHERE uuid = '?'");
+    $q->bind_param("s", $uuid);
+}
+
 $q->execute();
 $result = $q->get_result();
 
 if ($result->num_rows === 1) {
     $row = $result->fetch_assoc();
-    echo $row['uuid'];
+    if ($username) {
+        $uuid = $row['uuid'];
+        echo $uuid;
+    } else if ($uuid) {
+        echo $row['uuid'];
+    }
 } else {
     echo "FAIL";
 }
