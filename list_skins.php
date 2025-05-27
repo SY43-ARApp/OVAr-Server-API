af345d81f125bc4e57d02dfd24d0520b665aa771
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-05-27 10:00:33 +0200

    Add error reporting for debugging in list_skins.php

--- list_skins.php
+++ list_skins.php
@@ -1,4 +1,9 @@
 <?php
+// Debug: afficher les erreurs PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'DB.php';
 
 $resDir = __DIR__ . '/res/';
